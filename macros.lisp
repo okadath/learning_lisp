9817333e11e0f10bd00958595fbb3d4a0e316711
okadath <vintaw.01@gmail.com>
2020-05-29 19:25:31 -0500

    macroexpancion al aparecer es facil manejarlas con let pero si hay que usar setf no termino de comprender como funciona define-modify-macro internamente para modificar los setf (ni siquiera usa internamente setf, usa setq) pero al aprecer lo avienta directamente aun asi es asquerosamente sencillo escrbir macros que escriban HTML quiero pensar que lo mismo ocurrira con otros lenguajes ya que la sintaxis no es dificil mas que por los side effects de let y setf

--- macros.lisp
+++ macros.lisp
@@ -129,22 +129,254 @@
 		)
 	)
 
+; problemas con macros
+
+; macro mal definida
+(defmacro ntimes (n &rest body)
+	`(do ((x 0 (+ x 1)))
+		((>= x ,n))
+		,@body
+		)
+
+	)
+(nl)
+
+(print(macroexpand-1 '(ntimes 5)))
+
+; si la macro es llamada en un lugar que usa sus variables internas
+; puede haber errores
+(print
+(let ((x 10))
+	(ntimes 5
+		(setf x (+ x 1))
+		)
+	x
+	)
+)
+; deberia incrementar 5 y devolver 15
+;devuelve 10 por que existe x afuera y la macro lo usa
+; la macro es equivalente a esto
+(print
+(let ((x 10))
+	(DO ((X 0 (+ X 1))) ((>= X 5))
+		(setf x (+ x 1))
+		)
+	x
+	);devuelve 10
+)
+
+; incrementa a la x dentro del do, no la de let
+
+
+; la solucion mas general es no usar simbolos
+; que puedan ser capturados, mejor usamos gensyms
+; al no estar registrados esto no puede pasar
+
+(defmacro ntimes(n &rest body);mal
+	(
+		let ((g (gensym)))
+		`(do ((,g 0 (+ ,g 1)))
+			((>= ,g ,n))
+			,@body
+			)
+
+
+		)
+
+	)
+; esta macro aun es suceptible a multiples evaluaciones
+; como el primer argumento esta insertado en el do
+; sera evaluado en cada iteracion
+; esto se ve si hay side-effects
+(print
+(let ((x 10))
+	(ntimes 5
+		(setf x (+ x 1))
+		)
+	x
+	)
+);ya devuelve 15 pero aun esta mal escrita
+
+
+(let ((v 10))
+	(ntimes (setf v (- v 1))
+		(princ ".")
+		)
+	)
+
+
+(print (macroexpand-1 `(let ((v 10))
+				(ntimes (setf v (- v 1))
+					(princ ".")
+					)
+				)
+	)
+)
+
+(print (macroexpand-1'(ntimes (setf v (- v 1))
+					(princ ".")
+					)
+))
+
+
+; (LET ((V 10)) 
+; 	(
+; 		DO ((#:G3236 0 (+ #:G3236 1))
+; 			) 
+; 		((>= #:G3236 (SETF V (- V 1))))
+; 		 (PRINC "."))  
+; 	) 
+;
+
+;en las iteraciones se comparan las variables  contra #:GXXXX
+; no contra 9, 8,7...
+
+; esto se previene pasando la variable a otro gensym
+
+(defmacro ntimes (n &rest body)
+	(let ( 	(g (gensym))
+			(h (gensym))
+			)
+	`(
+		let ((,h ,n))
+		(do ((,g 0( +,g 1)))	
+			((>= ,g ,h))
+			,@body
+			)
+
+
+		)
+
+		)
+	)
+(print
+(let ((x 10))
+	(ntimes 5
+		(setf x (+ x 1))
+		)
+	x
+	)
+)
+
+(print "ya funciona")
+; siempre se deben usar gensyms en variables de side-effects
+(pprint (macroexpand `(let ((v 10))
+				(ntimes (setf v (- v 1))
+					(princ ".")
+					)
+				) 
+	)
+)
+(nl)
+(pprint (macroexpand-1'(NTIMES (SETF V (- V 1)) (PRINC "."))
+					
+))
+(nl)
+
+; (LET ((V 10)) 
+; 	(LET ((#:G3242 (SETF V (- V 1))))
+; 		(DO ((#:G3241 0 (+ #:G3241 1))) 
+; 			((>= #:G3241 #:G3242)) 
+; 			(PRINC ".")
+; 			)
+; 		) 
+; 	)
+
+; (LET ((V 10)) 
+; 	(
+; 		DO ((#:G3236 0 (+ #:G3236 1))
+; 			) 
+; 		((>= #:G3236 (SETF V (- V 1))))
+; 		 (PRINC "."))  
+; 	) 
+;
+
+(pprint (macroexpand-1 '(cond (a b)
+(c d e)
+(t f))))
+
+; en clisp solo hay macroexpand-1, hay que usar walkers(?)
+; para construir un expansor multiple
+; por el momento copypasteare
+
+
+; setf!!!!
+(defmacro cah (lst) 
+	`(car ,lst)
+	)
+
+
+(print
+(let ((x (list 'a 'b 'c)))
+	(Setf (cah x) 44)
+	x
+	)
+)
+; es facil usar setf por fuera, pero por dentro de la macro
+; es mas dificil
+
+; (defmacro incff(x &optional (y 1))
+; 	`(setf ,x (+ ,x ,y)); ni siquiera me lo permite
+; 	)
+; (pprint (macroexpand-1(incff 2 )))
+
+(define-modify-macro our-incf (&optional (y 1))
+	+
+	)
+
+; (pprint (macroexpand-1(our-incf 1)));? ni idea
+; al parecer debe ir aqui el setf
+; (our-incf 1 )
+(define-modify-macro appendif (val)
+	(lambda (lst val)
+		(append lst (list val))
+		)
+	)
+; (SETQ LST ((LAMBDA (LST VAL) (APPEND LST (LIST VAL))) LST 'D)) 
+
+
+
+(print
+(let ((lst '(a b c )))
+	(appendif lst 'd)
+	lst
+	)
+)
+
+(print(macroexpand '(appendif lst 'd)))
 
 
 
 
+(nl)
+(nl)
+(print(macroexpand '
+; ( let ((x 1))
+(our-incf x  22) 
+; x)
+)
+)
+; (SETQ X (+ X 22)) 
 
 
+; define-modify-macro al parecer internamente genera un setq
+; sin que nosotros lo debamos declarar
+; supongo que necesito practica para comprenderlo
+(nl)
+(print
+(loop for x = 8 then (/ x 2)
+until (< x 1)
+do (princ x))
+)
 
 
-
-
-
-
-
-
-
-
+(defmacro as (tag content)
+	`(format t "<~(~a~)>~a</~(~a~)>" ',tag ,content ',tag)
+	)
+(print (as center "asd"));:O
+; no se como define python las etiquetas
+; pero si es mas dificil que una macro
+; entonces lisp gana
 
 
 
